b1a5a91bb13ee169b282b7fc151639d7b5303b3b
happyxianyu <happyxianyu623@outlook.com>
2023-09-23 17:42:39 -0400

    s

--- xy/git_merge.py
+++ xy/git_merge.py
@@ -1,22 +1,30 @@
-from pyuu.path import Path
+merge_ignores = """
+.vscode
+xy
+tmp
+.gitignore
+
+""".strip().split('\n')
+
+
+
+
 import subprocess
+import os
+
+from pyuu.path import Path
+
+run_cmd = os.system
 
 self_path = Path(__file__)
 root_path = self_path.prnt.prnt
 
-# 读取排除文件列表
-with open(root_path/'.mergeignore', 'r') as f:
-    excluded_files = [line.strip() for line in f]
+os.chdir(root_path)
+run_cmd('git checkout xy-main')
+run_cmd('git merge xy')
+run_cmd('git reset main')
 
-# 合并分支，并排除指定文件
-def merge_with_exclusion(source_branch, target_branch):
-    for excluded_file in excluded_files:
-        subprocess.call(['git', 'checkout', target_branch, '--', excluded_file])
+for ignore in merge_ignores:
+    run_cmd(f'git reset -- "{Path(ignore).quote}"')
 
-    subprocess.call(['git', 'merge', source_branch])
 
-if __name__ == '__main__':
-    source_branch = 'xy'  # 源分支
-    target_branch = 'main'  # 目标分支
-
-    merge_with_exclusion(source_branch, target_branch)
